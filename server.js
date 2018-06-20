3c0313c5a3e77f6e5d2b4384e53b2c0f01a59dab
Anthony Edelstein <asedelstein@gmail.com>
2018-06-19 18:05:28 -0700

    heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(MONGODB_URI);
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
